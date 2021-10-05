f94e6e5e55a0b33b516e549bb83eda682d696d4b
Egor Astakhov <noise718@gmail.com>
2021-10-05 15:04:50 +0300

    Added Game model

---
+++ Apple Pie 2021/Game.swift
@@ -0,0 +1,15 @@
+//
+//  Game.swift
+//  Apple Pie 2021
+//
+//  Created by Егор Астахов on 05.10.2021.
+//
+
+import Foundation
+
+
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    
+}
